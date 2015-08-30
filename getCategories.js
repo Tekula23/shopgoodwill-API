bbfae4ecae267768f6f4103da2eddc4938d02716
robksawyer <robksawyer@gmail.com>
2015-08-30 16:58:09 -0700

    Minor tweaks

--- getCategories.js
+++ getCategories.js
@@ -4,34 +4,35 @@ var tidy = require('htmltidy').tidy;
 var searchUrl = "http://www.shopgoodwill.com/search/";
 
 exports.listCategories = function(req, res){
-  
-  request(searchUrl, function(err, resp, body) {
-    if(!err) {
-      tidy(body, function(error, html){
-        if(!error) {
-          getCategories(html);  
-        }
-      });
-    }
-  });  
-  
-  var categoriesArray = [];
 
-  var getCategories = function(html){
-    $ = cheerio.load(html);
-    var catOptions = $('select#catid').children('option');
-    
-    catOptions.each(function(i, el){
-      var catName = $(el).html();
-      var catID = $(el).val();
-      categoriesArray.push({catName : catName, catId : catID});
-      // if(catName.indexOf("&gt;") < 0) {
-      //   categoriesArray.push({name : catName, id : catID});  
-      // };
-    });
-    
-    res.jsonp(categoriesArray);
+	request(searchUrl, function(err, resp, body) {
+		if(!err) {
+			tidy(body, function(error, html){
+				if(!error) {
+					getCategories(html);
+				}
+			});
+		}
+	});
 
-  };
+	var categoriesArray = [];
 
+	var getCategories = function(html){
+		$ = cheerio.load(html);
+		var catOptions = $('select#catid').children('option');
+
+		catOptions.each(function(i, el){
+			var catName = $(el).html();
+			var catID = $(el).val();
+			categoriesArray.push({
+				id : catID,
+				title : catName
+			});
+			// if(catName.indexOf("&gt;") < 0) {
+			//	categoriesArray.push({name : catName, id : catID});
+			// };
+		});
+
+		res.jsonp(categoriesArray);
+	};
 };
