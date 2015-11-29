4718094d7a8d9e1a3b76f8ca5a2b638eaef11d31
Rob Sawyer <robksawyer@gmail.com>
2015-11-28 20:15:39 -0800

    Fixed an issue with auction timing.

--- routes/getAuction.js
+++ routes/getAuction.js
@@ -1,7 +1,7 @@
 var cheerio	 = require('cheerio');
 var request	 = require('request');
 var tidy	= require('htmltidy').tidy;
-var moment	= require('moment');
+var moment = require('moment-timezone');
 var url	 = require('url');
 var http	= require('http');
 var ua 				= require('universal-analytics');
@@ -15,6 +15,9 @@ exports.viewAuction = function(req, res){
 	var queryId = "";
 	var itemURL = "";
 	var visitor = ua(process.env.GA_UA);
+
+	//Set default timezone
+	moment.tz.setDefault("America/Los_Angeles");
 
 	if(req.query.id) {
 		queryId = req.query.id;
