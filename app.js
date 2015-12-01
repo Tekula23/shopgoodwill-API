80994485df3da781d62ba4e2febdcc48889921a0
Rob Sawyer <robksawyer@gmail.com>
2015-11-30 19:26:37 -0800

    Set the header encoding.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var socket 	= require('socket.io');
 var visitor = ua(process.env.GA_UA);
 var iconv 	= require('iconv-lite');
 
-// var encoding = 'utf-8';//'windows-1252';
+var encoding = 'utf-8';//'windows-1252';
 
 app.listen(port, function() {
 	console.log("Listening on " + port);
@@ -19,7 +19,7 @@ app.all("/*", function(req, res, next){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
 	res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-	// res.header("Content-Type", "application/json; charset=" + encoding);
+	res.header("Content-Type", "application/json; charset=" + encoding);
 	visitor.pageview(req.originalUrl).send();
 	// console.log("--- UA: Visitor ---");
 	// console.log(visitor.debug());
